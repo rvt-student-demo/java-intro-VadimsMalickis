3cfa2c6ed3b8ab58746f9dbe8ab57d758f4d9b7d
Vadims Malickis <117085106+VadimsMalickis@users.noreply.github.com>
2024-09-03 08:12:34 +0000

    fix config

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,31 +1,11 @@
 package lv.rvt;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-
 public class App 
 {
     public static void main( String[] args )
     {
-        long lastId = -1;
-        try (BufferedReader reader = Utils.getReader("data.csv")) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        try (BufferedWriter writter = Utils.getWriter("data.csv")) {
-            // writter.newLine();
-            // writter.write("2, user, 123");
-            // writter.flush();
-        
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
+       
+        System.out.println("Hello world!");
 
     }
 
