7d720c31fdd8212b53c69ace50eb22954184ba85
VadimsMalickis <vadims.malickis@rvt.lv>
2024-09-24 08:57:55 +0300

    update template

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,15 +4,7 @@ public class App
 {
     public static void main( String[] args ) {
 
-        // My code starts here
-        String name = "Jānis";
-        int age = 20;
-        boolean isOnline = false;
-
-        System.out.println(age);
-
-
-        // Code ends
+        // Gift tax
 
     }
 
