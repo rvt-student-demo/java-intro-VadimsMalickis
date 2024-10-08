22316456b94d5d129bcff408de3ae679e4b51938
VadimsMalickis <vadims.malickis@rvt.lv>
2024-10-08 10:44:05 +0300

    Callstack demontration

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,9 +3,25 @@ package lv.rvt;
 public class App 
 {
     public static void main( String[] args ) {
-
-        // Gift tax
-
+        System.out.println("main()");
+        printStars(0);
+        System.out.println("Program end..");
     }
 
+    public static void printStars(int number) {
+        System.out.println("printStarts()");
+        printSquare(0);
+
+    }
+    public static void printSquare(int size) {
+        System.out.println("printSquare()");
+        printRectangle(0, 0);
+    }
+    public static void printRectangle(int width, int height) {
+        System.out.println("printRectangle()");
+        printTriangle(0);
+    }
+    public static void printTriangle(int size) {
+        System.out.println("printTriangle()");
+    }
 }
