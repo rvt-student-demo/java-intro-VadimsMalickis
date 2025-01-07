f6554f1f64765d18d6323e81abb633f6a98c5f8a
VadimsMalickis <vadims.malickis@rvt.lv>
2025-01-07 15:14:04 +0200

    minor changes

---
+++ src/main/java/lv/rvt/Main.java
@@ -0,0 +1,25 @@
+package lv.rvt;
+
+public class Main 
+{
+
+    public static void main(String[] args) throws Exception {  
+        
+
+    }
+
+    
+    private static void modify(Person person) {
+        person.setName("Modified!");
+    }
+
+
+    private static void modify(int value) {
+        value += 5;
+    }
+
+  
+
+   
+
+}
