8c5fa4685c664b6d5b663ff7db48df8c688907da
VadimsMalickis <vadims.malickis@rvt.lv>
2025-01-08 10:52:30 +0200

    classwork

--- src/main/java/lv/rvt/Main.java
+++ src/main/java/lv/rvt/Main.java
@@ -5,7 +5,9 @@ public class Main
 
     public static void main(String[] args) throws Exception {  
         
+        Person person = new Person("John");
 
+        System.out.println(person instanceof Object);
     }
 
     
