8e352f17183db43cb1af2c97c8ec195895b233b6
VadimsMalickis <vadims.malickis@rvt.lv>
2025-09-15 12:17:59 +0300

    Testa kommits

--- src/main/java/lv/rvt/Main.java
+++ src/main/java/lv/rvt/Main.java
@@ -8,12 +8,7 @@ public class Main
 {
     public static void main(String[] args) throws Exception {  
 
-        Card crd;
-        // bd mainīgajā mums is saglabāta adrese uz objektu
-        Birthday bd = new Birthday("Lucinda", 42);
-
-        crd = bd;
-
+        
     }   
     
 }
